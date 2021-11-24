00d163a7a0d31511d5afd76be1b58872e2a1280b
Alessandra Chardon <achardon@ucla.edu>
2021-11-24 15:38:02 -0500

    finished lecture activity

--- movies_app/lib/cli.rb
+++ movies_app/lib/cli.rb
@@ -1,16 +1,18 @@
 
 
 def initialize_app 
+    seed
     menu
-    # tickets = [{name:'rose', movie:'2001: A Space Odyssey', price:5.00},{name:'ix', movie:'The Godfather', price:5.00}]
 end 
 
 def menu
     puts "Welcome to Flatiron Movies"
     puts "Please choose an option:"
     puts "1. List all Movies"
-    puts "2. List Tickte holder names"
-    puts "3. Add ticket"
+    puts "2. List all tickets"
+    puts "3. Add Ticket"
+    puts "4. Find Tickets"
+    puts "5. Discount"
     puts "0. to exit"
     
     user_input = gets.strip
@@ -20,10 +22,27 @@ def menu
         movies = ['2001: A Space Odyssey', 'The Godfather', 'Citizen Kane', 'Raiders of the Lost Ark', 'In the mood for love', 'Seven Samurai']
 
         movies.each {|movie| puts movie }
+        return_to_menu
     when "2"
-       puts Ticket.all.map {|ticket| ticket.name }
+       puts Ticket.all.map {|ticket| ticket.print_ticket }
+       return_to_menu
     when "3"
         puts create_ticket
+        return_to_menu
+    when "4"
+        puts "Enter movie name"
+        movie = gets.strip
+        puts Ticket.find_tickets(movie)
+        return_to_menu
+    when "5"
+        puts "What is your name"
+        name = gets.strip
+        puts "What Movie"
+        title = gets.strip
+
+        ticket = Ticket.all.find{|t| t.name == name && t.title == title}
+        ticket.discount
+        return_to_menu
     else 
         puts "goodbye"
     end 
@@ -37,3 +56,15 @@ def create_ticket
     t1 = Ticket.new(name, title)
     menu
 end 
+
+def seed
+    Ticket.new('rose','2001: A Space Odyssey')
+    Ticket.new('ix', 'The Godfather')
+    Ticket.new('enzo','Hamilton')
+end
+
+def return_to_menu
+    puts "press any button to return to menu"
+    gets.strip
+    menu
+end
